4a1a6b29395f36b903f25f038711be95a20fb463
Daniel Cordero <dn.lambb@gmail.com>
2023-10-22 23:39:11 -0300

    feat: add choice button to select create a task or a stage.

--- lib/screens/ProjectsScreen.dart
+++ lib/screens/ProjectsScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -28,9 +30,9 @@ class _ProjectsScreenState extends State<ProjectsScreen> {
     });
   }
 
-  void _createProject(String projectName, String projectDescription) {
+  void _createProject(String projectName) {
     setState(() {
-      projects.add(Project(name: projectName, description: projectDescription));
+      projects.add(Project(name: projectName));
     });
   }
 
@@ -67,7 +69,7 @@ class _ProjectsScreenState extends State<ProjectsScreen> {
 }
 
 class CreateProjectForm extends StatefulWidget {
-  final void Function(String projectName, String projectDescription) onSubmit;
+  final void Function(String projectName) onSubmit;
 
   const CreateProjectForm({super.key, required this.onSubmit});
 
@@ -77,7 +79,6 @@ class CreateProjectForm extends StatefulWidget {
 
 class _CreateProjectFormState extends State<CreateProjectForm> {
   final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -88,11 +89,7 @@ class _CreateProjectFormState extends State<CreateProjectForm> {
         children: <Widget>[
           TextFormField(
             controller: _nameController,
-            decoration: const InputDecoration(labelText: 'Nombre'),
-          ),
-          TextFormField(
-            controller: _descriptionController,
-            decoration: const InputDecoration(labelText: 'Descripción'),
+            decoration: const InputDecoration(labelText: 'Nombre del proyecto'),
           ),
         ],
       ),
@@ -106,15 +103,13 @@ class _CreateProjectFormState extends State<CreateProjectForm> {
         ElevatedButton(
           onPressed: () {
             final projectName = _nameController.text;
-            final projectDescription = _descriptionController.text;
-            widget.onSubmit(projectName, projectDescription);
+            widget.onSubmit(projectName);
 
             _nameController.clear();
-            _descriptionController.clear();
 
             Navigator.of(context).pop();
           },
-          child: const Text('Crear proyecto'),
+          child: const Text('Crear Proyecto'),
         ),
       ],
     );
@@ -193,52 +188,59 @@ class ProjectDetailScreen extends StatefulWidget {
 class _ProjectDetailScreenState extends State<ProjectDetailScreen> {
   final TextEditingController _nameController = TextEditingController();
 
+  String? selectedStage;
+
   @override
   void initState() {
     super.initState();
     _nameController.text = widget.project.name;
+    if (widget.project.stages.isNotEmpty) {
+      selectedStage = widget.project.stages.first.name;
+    }
   }
 
-  void _showCreateStageForm(BuildContext context) {
-    final TextEditingController stageNameController = TextEditingController();
+  void _showCreateStageOrTaskForm(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Crear Etapa'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextFormField(
-                controller: stageNameController,
-                decoration:
-                    const InputDecoration(labelText: 'Nombre de la etapa'),
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cancelar'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                final stageName = stageNameController.text;
-                if (stageName.isNotEmpty) {
-                  setState(() {
-                    widget.project.stages.add(Stage(name: stageName));
-                  });
-                }
-                Navigator.of(context).pop();
-              },
-              child: const Text('Crear Etapa'),
-            ),
-          ],
+        return CreateStageOrTaskForm(
+          stages: widget.project.stages,
+          onCreateStage: _createStage,
+          onCreateTask: (taskName, stageName) {
+            _createTask(taskName, stageName);
+          },
         );
       },
     );
+  }
+
+  void _createStage(String stageName) {
+    setState(() {
+      widget.project.stages.add(Stage(name: stageName, tasks: []));
+    });
+    Navigator.of(context).pop();
+  }
+
+  void _createTask(String taskName, String stageName) {
+    setState(() {
+      if (selectedStage != null) {
+        final stage =
+            widget.project.stages.firstWhere((s) => s.name == selectedStage);
+        stage.tasks.add(Task(name: taskName));
+      }
+    });
+    Navigator.of(context).pop();
+  }
+
+  void _deleteTask(Stage stage, Task task) {
+    setState(() {
+      final stageIndex = widget.project.stages.indexOf(stage);
+      if (stageIndex != -1) {
+        final updatedTasks = stage.tasks.where((t) => t != task).toList();
+        final updatedStage = stage.copyWith(tasks: updatedTasks);
+        widget.project.stages[stageIndex] = updatedStage;
+      }
+    });
   }
 
   void _deleteStage(Stage stage) {
@@ -291,43 +293,12 @@ class _ProjectDetailScreenState extends State<ProjectDetailScreen> {
           ),
           IconButton(
             icon: const Icon(Icons.edit),
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text('Editar Nombre del Proyecto'),
-                    content: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        TextFormField(
-                          controller: _nameController,
-                          decoration:
-                              const InputDecoration(labelText: 'Nuevo nombre'),
-                        ),
-                      ],
-                    ),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Cancelar'),
-                      ),
-                      ElevatedButton(
-                        onPressed: _saveName,
-                        child: const Text('Guardar'),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
+            onPressed: () {},
           ),
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
-              _showCreateStageForm(context);
+              _showCreateStageOrTaskForm(context);
             },
           ),
         ],
@@ -337,20 +308,6 @@ class _ProjectDetailScreenState extends State<ProjectDetailScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'Descripción del proyecto:',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(4)),
-            Text(
-              widget.project.description,
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 16),
             const Text(
               'Etapas:',
               style: TextStyle(
@@ -365,13 +322,45 @@ class _ProjectDetailScreenState extends State<ProjectDetailScreen> {
                 itemCount: widget.project.stages.length,
                 itemBuilder: (context, index) {
                   final stage = widget.project.stages[index];
-                  return ListTile(
-                    title: Text(stage.name),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete),
-                      onPressed: () {
-                        _deleteStage(stage);
-                      },
+                  return Card(
+                    margin: const EdgeInsets.all(8.0),
+                    child: Container(
+                      color: Colors.blue[100],
+                      child: Column(
+                        children: <Widget>[
+                          ListTile(
+                            title: Text(stage.name),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () {
+                                _deleteStage(stage);
+                              },
+                            ),
+                          ),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: stage.tasks.length,
+                            itemBuilder: (context, taskIndex) {
+                              final task = stage.tasks[taskIndex];
+                              return Card(
+                                color: Colors.white,
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: ListTile(
+                                  title: Text(task.name),
+                                  trailing: IconButton(
+                                    icon: const Icon(Icons.delete),
+                                    onPressed: () {
+                                      _deleteTask(stage, task);
+                                    },
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                          const Padding(padding: EdgeInsets.all(15))
+                        ],
+                      ),
                     ),
                   );
                 },
@@ -384,21 +373,98 @@ class _ProjectDetailScreenState extends State<ProjectDetailScreen> {
   }
 }
 
+class CreateStageOrTaskForm extends StatefulWidget {
+  final List<Stage> stages;
+  final void Function(String) onCreateStage;
+  final void Function(String, String) onCreateTask;
+
+  const CreateStageOrTaskForm(
+      {super.key,
+      required this.stages,
+      required this.onCreateStage,
+      required this.onCreateTask});
+
+  @override
+  _CreateStageOrTaskFormState createState() => _CreateStageOrTaskFormState();
+}
+
+class _CreateStageOrTaskFormState extends State<CreateStageOrTaskForm> {
+  final TextEditingController _nameController = TextEditingController();
+  bool _isCreatingTask = false;
+  String? selectedStage;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(_isCreatingTask ? 'Crear Tarea' : 'Crear Etapa'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          if (_isCreatingTask)
+            DropdownButton<String>(
+              value: selectedStage,
+              items: widget.stages.map((stage) {
+                return DropdownMenuItem<String>(
+                  value: stage.name,
+                  child: Text(stage.name),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedStage = value;
+                });
+              },
+            ),
+          TextFormField(
+            controller: _nameController,
+            decoration: InputDecoration(
+              labelText:
+                  _isCreatingTask ? 'Nombre de la tarea' : 'Nombre de la etapa',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final name = _nameController.text;
+              if (_isCreatingTask) {
+                final stageName = selectedStage ?? "sin nombre";
+                widget.onCreateTask(name, stageName);
+              } else {
+                widget.onCreateStage(name);
+              }
+
+              _nameController.clear();
+              Navigator.of(context).pop();
+            },
+            child: Text(_isCreatingTask ? 'Crear Tarea' : 'Crear Etapa'),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            setState(() {
+              _isCreatingTask = !_isCreatingTask;
+            });
+          },
+          child: Text(_isCreatingTask ? 'Crear Etapa' : 'Crear Tarea'),
+        ),
+      ],
+    );
+  }
+}
+
 class Project {
   final String name;
-  final String description;
   final List<Stage> stages;
 
   Project({
     required this.name,
-    required this.description,
     List<Stage>? stages,
   }) : stages = stages ?? [];
 
   Project copyWith({String? name, String? description, List<Stage>? stages}) {
     return Project(
       name: name ?? this.name,
-      description: description ?? this.description,
       stages: stages ?? this.stages,
     );
   }
@@ -406,6 +472,20 @@ class Project {
 
 class Stage {
   final String name;
+  final List<Task> tasks;
 
-  Stage({required this.name});
+  Stage({required this.name, List<Task>? tasks}) : tasks = tasks ?? [];
+
+  Stage copyWith({List<Task>? tasks}) {
+    return Stage(
+      name: this.name,
+      tasks: tasks ?? this.tasks,
+    );
+  }
+}
+
+class Task {
+  final String name;
+
+  Task({required this.name});
 }
