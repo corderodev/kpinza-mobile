537e3b18845a3964b8a73d69ecf43129fc6cadd7
Daniel Cordero <dn.lambb@gmail.com>
2023-12-07 03:41:51 -0300

    add logout confirmation dialog

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -7,7 +7,7 @@ import 'package:kpinza_mobile/screens/NotificationScreen.dart';
 import 'package:kpinza_mobile/screens/ProjectsScreen.dart';
 
 class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
+  const MainScreen({Key? key});
 
   @override
   State<MainScreen> createState() => _MainScreenState();
@@ -22,6 +22,36 @@ class _MainScreenState extends State<MainScreen> {
     });
   }
 
+  Future<void> _showLogoutConfirmationDialog() async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmación'),
+          content: const Text('¿Estás seguro de que deseas cerrar la cuenta?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const AuthScreen(),
+                  ),
+                );
+              },
+              child: const Text('Cerrar cuenta'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,20 +59,20 @@ class _MainScreenState extends State<MainScreen> {
         title: const Text('Kpinza'),
         actions: <Widget>[
           IconButton(
-            icon: const Icon(Icons.logout), // Icono para el botón
+            icon: const Icon(Icons.logout),
             onPressed: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(
-                  builder: (context) => const AuthScreen(),
-                ),
-              );
+              _showLogoutConfirmationDialog();
             },
           ),
         ],
       ),
       body: IndexedStack(
         index: _currentIndex,
-        children: [DataScreen(), ProjectsScreen(), NotificationScreen()],
+        children: [
+          const DataScreen(),
+          const ProjectsScreen(),
+          const NotificationScreen()
+        ],
       ),
       bottomNavigationBar: kpinza_component.NavigationBar(
         onTabTapped: _onTabTapped,
